8c50a9bad79772449ca143b1360633608d7135f8
Bens-o <benso8650@gmail.com>
2026-01-25 16:52:25 +0900

    Experimenting with plotting a portion of a large dataset

--- .venv/Classification.py
+++ .venv/Classification.py
@@ -4,7 +4,23 @@ import matplotlib.pyplot as plt
 
 classi = io.loadmat('classi.mat')
 
+# Loading .mat file of neurone data as 2 matrices
 cat1 = classi['cat1']
 cat2 = classi['cat2']
 
+print(np.size(cat1, 0))
+
+# Number of neurones
+numN = np.arange(0, np.size(cat1, 0))
+
+print(numN)
+
+
+plt.plot(cat1[:, 0],numN, 'o')
+
+plt.xticks(np.arange(0,1, step=0.01))
+plt.yticks(np.arange(0, np.size(numN), step=5))
+
+
+plt.show()
 
